41ed45bc74b15a11fa7dc8d602e4bd9a436773e9
Adachi Izumi <izugch73@gmail.com>
2024-09-02 17:52:21 +0900

    fix tests

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,41 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
+    [SerializeField] public HyphenationJpn _hypJpn;
 
-	public HyphenationJpn hypJpn;
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, 600, 600));
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        GUILayout.Label("width");
+        _hypJpn.TextWidth = GUILayout.HorizontalSlider(_hypJpn.TextWidth, 100, 600);
+        GUILayout.Label("fontSize");
+        _hypJpn.FontSize = (int)GUILayout.HorizontalSlider(_hypJpn.FontSize, 10, 40);
 
-	void OnGUI()
-	{
-		GUILayout.BeginArea(new Rect(10,10,200,200));
-		
-//		RectTransform rectTrans = hypJpn.GetComponent<RectTransform>();
-//		Rect rect = rectTrans.rect;
-//		rect.width = GUILayout.HorizontalSlider(rect.width, 100, 600);
-//		rectTrans.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rect.width);
+        GUILayout.Space(20);
 
-		GUILayout.Label("width");
-		hypJpn.textWidth = GUILayout.HorizontalSlider(hypJpn.textWidth, 100, 600);
-		GUILayout.Label("fontSize");
-		hypJpn.fontSize = (int)GUILayout.HorizontalSlider(hypJpn.fontSize, 10, 40);
+        if (GUILayout.Button("ChangeText"))
+        {
+            const string sampleText = "Unityマニュアルガイドは特定のプラットフォームにのみ適用されるセクションを含みます。\n自ら参照したいセクションを選択して下さい。プロットフォーム特有の情報は各ページの三角形の記号で示されるボタンにより展開して参照することが出来ます。";
+            _hypJpn.SetText(sampleText);
+        }
 
-		GUILayout.Space(20);
-
-		if(GUILayout.Button("ChangeText")){
-			string sampleText = "Unityマニュアルガイドは特定のプラットフォームにのみ適用されるセクションを含みます。\n自ら参照したいセクションを選択して下さい。プロットフォーム特有の情報は各ページの三角形の記号で示されるボタンにより展開して参照することが出来ます。";
-			hypJpn.SetText(sampleText);
-		}
-
-		GUILayout.EndArea();
-	}
+        GUILayout.EndArea();
+    }
 }
